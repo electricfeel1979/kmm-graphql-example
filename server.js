703e0ec5d9441e220fd465fa92489d312f75a2e5
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-01-02 18:26:51 +0800

    282. GraphQL In Node - 11min

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = buildSchema(`
+    type Query {
+        description: String
+        price: Float
+    }
+`);
+
+const root = {
+    description: 'Red Shoe',
+    price: 42.12
+};
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    root
+}));
+
+app.listen(3000, () => {
+    console.log('Running GraphQl server...');
+})
